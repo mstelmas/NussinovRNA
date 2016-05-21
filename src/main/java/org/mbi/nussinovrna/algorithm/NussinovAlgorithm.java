edf3ea4837b9039d6d578c93594da837571be959
mstelmas <marcinstel.code@gmail.com>
2016-05-21 20:19:11 +0200

    Rearranged project structure one more time

--- src/main/java/org/mbi/nussinovrna/algorithm/NussinovAlgorithm.java
+++ src/main/java/org/mbi/nussinovrna/algorithm/NussinovAlgorithm.java
@@ -3,7 +3,7 @@ package org.mbi.nussinovrna.algorithm;
 import org.mbi.nussinovrna.rna.RnaSecondaryStruct;
 import org.mbi.nussinovrna.rna.RnaSequence;
 
-public final class NussinovAlgorithm implements SecondaryStructAlgorithm {
+public final class NussinovAlgorithm implements org.mbi.nussinovrna.algorithm.SecondaryStructAlgorithm {
 
     @Override
     public RnaSecondaryStruct execute(final RnaSequence rnaSequence) {
